fa9ddb2d59deffc68fae43403b153cf79e9af017
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2019-12-13 21:21:12 +0100

    Fix String -> Expression in Java Delegate

--- src/main/kotlin/me/kcybulski/fixit/bpmn/service/EmailServiceTask.kt
+++ src/main/kotlin/me/kcybulski/fixit/bpmn/service/EmailServiceTask.kt
@@ -4,21 +4,30 @@ import me.kcybulski.fixit.bpmn.SpringContext
 import me.kcybulski.fixit.domain.EmailRequest
 import me.kcybulski.fixit.domain.EmailSender
 import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.Expression
 import org.camunda.bpm.engine.delegate.JavaDelegate
 
 class EmailServiceTask: JavaDelegate {
 
-    private lateinit var senderEmail: String
-    private lateinit var senderName: String
-    private lateinit var receiverEmail: String
-    private lateinit var receiverName: String
-    private lateinit var subject: String
-    private lateinit var text: String
+    private lateinit var senderEmail: Expression
+    private lateinit var senderName: Expression
+    private lateinit var receiverEmail: Expression
+    private lateinit var receiverName: Expression
+    private lateinit var subject: Expression
+    private lateinit var text: Expression
 
     override fun execute(execution: DelegateExecution) {
-        SpringContext.getBean(EmailSender::class.java).sendEmail(
-                EmailRequest(senderEmail, senderName, receiverEmail, receiverName, subject, text)
-        )
+        SpringContext.getBean(EmailSender::class.java).sendEmail(getRequest(execution))
     }
 
+    private fun getRequest(execution: DelegateExecution) = EmailRequest(
+            senderEmail.toString(execution),
+            senderName.toString(execution),
+            receiverEmail.toString(execution),
+            receiverName.toString(execution),
+            subject.toString(execution),
+            text.toString(execution))
+
 }
+
+fun Expression.toString(execution: DelegateExecution) = getValue(execution) as String
